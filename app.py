1a5a28ba9ab419837a3501ac6ab2f301e17f8d15
yash <ycjaware@gmail.com>
2024-01-26 18:26:51 +0530

    Initial commit

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain.vectorstores import FAISS
+# from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
+from langchain_community.vectorstores import FAISS
 
 load_dotenv()
 os.getenv("GOOGLE_API_KEY")
